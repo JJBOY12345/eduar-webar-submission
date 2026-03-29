48adf3c44f74b3e7aefdd7b441b5c615b7348845
JJBOY-CODES <jeswinjoel2470029@ssn.edu.in>
2026-03-29 16:43:25 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/eduar-webar-submission/",
   server: {
     allowedHosts: ["lacie-unnosed-uncontumaciously.ngrok-free.dev"]
   }
